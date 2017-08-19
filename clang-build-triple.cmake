eeeb9f58290cda9ecb2fb1f12896baa81116332b
Zac Hansen <xaxxon@gmail.com>
2017-08-18 18:25:17 -0700

    added some extra extra error checking and comments

--- clang-build-triple.cmake
+++ clang-build-triple.cmake
@@ -1,7 +1,21 @@
 
+# Get the target triple for the current host by calling clang -v and then stripping out the Target: value from its output
+
+# $ clang -v
+# clang version 4.0.0 (tags/RELEASE_400/final)
+# Target: x86_64-apple-darwin15.6.0
+# Thread model: posix
+# InstalledDir: /Users/xaxxon/Downloads/clang+llvm-4.0.0-x86_64-apple-darwin/bin
+
+
 if(UNIX)
-	# Get the target triple for the current host by calling clang -v and then stripping out the Target: value from its output
+    # Store all the results of running `clang -v` into CLANG_VERSION_INFO
     execute_process(COMMAND ${CMAKE_CXX_COMPILER} -v ERROR_VARIABLE CLANG_VERSION_INFO)
+
+    # Parse out the target triple from the raw output of `clang -v`
+    # cmake regexes are missing a lot of common syntax options for regexes
     string(REGEX REPLACE ".*Target:[\r\n\t ]*([^\r\n\t]*).*Thread model.*" "\\1" TARGET_TRIPLE ${CLANG_VERSION_INFO})
-    message(STATUS "TARGET TRIPLE: '${TARGET_TRIPLE}' END")
+
+
+    message(STATUS "clang -target value: '${TARGET_TRIPLE}'")
 endif()
--- v8.cmake
+++ v8.cmake
@@ -6,6 +6,9 @@ set(V8_LIB_NAMES icui18n icuuc v8 v8_libbase v8_libplatform)
 
 FOREACH(LIB_NAME ${V8_LIB_NAMES})
     FIND_LIBRARY(FOUND_LIB_${LIB_NAME} ${LIB_NAME} PATHS ${V8_LIB_DIR})
+    IF(NOT FOUND_LIB_${LIB_NAME})
+        message(FATAL_ERROR "${LIB_NAME} NOT FOUND")
+    ENDIF()
     add_library(v8::${LIB_NAME} UNKNOWN IMPORTED)
     set_target_properties(v8::${LIB_NAME} PROPERTIES
             IMPORTED_LOCATION "${FOUND_LIB_${LIB_NAME}}"
@@ -13,6 +16,6 @@ FOREACH(LIB_NAME ${V8_LIB_NAMES})
             INTERFACE_INCLUDE_DIRECTORIES ${V8_INCLUDE_DIR})
 
     LIST(APPEND V8_LIBS v8::${LIB_NAME})
-    MESSAGE("Lib: v8::${LIB_NAME}")
-    MESSAGE("Found Lib: ${FOUND_LIB_${LIB_NAME}}")
+    #MESSAGE("Lib: v8::${LIB_NAME}")
+    #MESSAGE("Found Lib: ${FOUND_LIB_${LIB_NAME}}")
 ENDFOREACH(LIB_NAME)
