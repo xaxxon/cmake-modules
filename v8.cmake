4fc28defdaa602d37cf41b98adebd9009a2f1c42
Zac Hansen <xaxxon@gmail.com>
2019-03-18 18:09:27 -0700

    changed expected v8 lib layout to have the libs and headers in the same general location - now needs V8_DIR and V8_BUILD_TYPE instead of just V8_LIB_DIR and looking for headers elsewhere

--- v8.cmake
+++ v8.cmake
@@ -1,21 +1,48 @@
 
+# V8_LIB_DIR is the base dir for the version to use.
+#   - it should have an 'include' dir that has the proper include files for all builds of the version
+#   - and directories for each build available, such as x64.release, x64.debug, etc. 
+
+
+IF (NOT V8_DIR)
+    message(FATAL_ERROR, "V8_DIR not specified, cannot find v8 library files")
+    return()
+ENDIF()
+
+IF (NOT IS_DIRECTORY ${V8_DIR})
+    message(FATAL_ERROR, "Specified V8_DIR '${V8_DIR}' isn't a directory")
+    return()
+ENDIF()
+
+IF (NOT V8_BUILD_TYPE)
+   message(FATAL_ERROR, "V8_BUILD_TYPE not specified, cannot determine build variant to use (e.g. x64.release)")
+   return()
+ENDIF()
+
+
+
+set(V8_INCLUDE_DIR "${V8_DIR}/include")
+set(V8_LIB_DIR "${V8_DIR}/${V8_BUILD_TYPE}")
+
+IF (NOT EXISTS "${V8_INCLUDE_DIR}/v8.h")
+    message(FATAL_ERROR, "Could not find v8.h in ${V8_INCLUDE_DIR}")
+    return()
+ENDIF()
+
+IF (NOT IS_DIRECTORY ${V8_LIB_DIR})
+    message(FATAL_ERROR, "Build type directory '${V8_LIB_DIR}' doesn't exist")
+    return()
+ENDIF()
+
+
+message("looking for v8 libs in ${V8_LIB_DIR} for ${V8_BUILD_TYPE}")
+
 set(V8_LIB_NAMES icui18n icuuc v8 v8_libbase v8_libplatform)
 
-IF(${V8_INCLUDE_DIR} STREQUAL "")
-    message(FATAL_ERROR "No V8_INCLUDE_DIR specified")
-ENDIF()
-IF(${V8_LIB_DIR} STREQUAL "")
-    message(FATAL_ERROR "No V8_LIB_DIR specified")
-ENDIF()
-
-message("looking for v8 libs in ${V8_LIB_DIR} and headers in ${V8_INCLUDE_DIR}")
-
-
 FOREACH(LIB_NAME ${V8_LIB_NAMES})
-    message("Finding v8 library: ${LIB_NAME}")
     FIND_LIBRARY(FOUND_LIB_${LIB_NAME} ${LIB_NAME} PATHS ${V8_LIB_DIR})
     IF(NOT FOUND_LIB_${LIB_NAME})
-        message(FATAL_ERROR "${LIB_NAME} NOT FOUND in V8_LIB_DIR: '${V8_LIB_DIR}'")
+        message(FATAL_ERROR "${LIB_NAME} NOT FOUND")
     ENDIF()
     add_library(v8::${LIB_NAME} UNKNOWN IMPORTED)
     set_target_properties(v8::${LIB_NAME} PROPERTIES
